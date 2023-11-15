77f866e7d39d0881504d1879112a5be51ce9348e
alexander <alexanderhardeland@gmail.com>
2023-11-15 17:00:20 +0100

    I dont know what happend, but after the last commit. All files and dirs was deleted and came back again

---
+++ src/PageApplication.tsx
@@ -0,0 +1,9 @@
+export default function PageApplication() {
+  return (
+    <div>
+      <header>
+        <h1>Hello World</h1>
+      </header>
+    </div>
+  );
+}
---
+++ src/test/App.test.tsx
@@ -0,0 +1,7 @@
+import { render, screen } from "@testing-library/react";
+import PageApplication from "../PageApplication";
+test("renders learn react link", () => {
+  render(<PageApplication />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
