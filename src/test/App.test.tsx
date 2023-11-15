b85083baf7334511cc696a819316bde137994028
alexander <alexanderhardeland@gmail.com>
2023-11-15 16:46:53 +0100

    Created four new dirs in ./src and moved two test files over in the new test directory

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import PageApplication from "./PageApplication";
+import PageApplication from "../PageApplication";
 test("renders learn react link", () => {
   render(<PageApplication />);
   const linkElement = screen.getByText(/learn react/i);
