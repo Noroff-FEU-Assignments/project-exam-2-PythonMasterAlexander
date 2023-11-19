75b8f76816c86494cc089f76c59bed7c047511b8
alexander <alexanderhardeland@gmail.com>
2023-11-19 16:06:25 +0100

    Working on getting Tailwind up and running but so far I have not managed to.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import PageApplication from "./PageApplication";
+import "./styles/style.css";
 import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,7 +1,9 @@
 export default function LandingPage() {
   return (
     <>
-      <main></main>
+      <main className="bg-blue-500 text-white p-4">
+        <h1 className="text-3xl font-bold underline">Hello World</h1>
+      </main>
     </>
   );
 }
