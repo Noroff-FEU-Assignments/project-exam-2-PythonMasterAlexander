7453ca48c004a2093df5e3e9fc3ebefebd1b57dc
Alexander <alexanderhardeland@gmail.com>
2023-11-28 13:12:52 +0000

    Adds a new page UserLandingPage, changed name on UserHomePage to CreateUserPage

--- src/PageApplication.tsx
+++ src/PageApplication.tsx
@@ -1,5 +1,7 @@
 import LandingPage from "./pages/LandingPage";
 import PageLayout from "./components/PageLayout";
+import UserLandingPage from "./pages/UserLandingPage";
+import CreateUserPage from "./pages/CreateUserPage";
 import "./tailwind.css";
 import { Routes, Route } from "react-router-dom";
 export default function PageApplication() {
@@ -7,6 +9,8 @@ export default function PageApplication() {
     <Routes>
       <Route path="/" element={<PageLayout />}>
         <Route index element={<LandingPage />} />
+        <Route element={<UserLandingPage />} />
+        <Route element={<CreateUserPage />} />
       </Route>
     </Routes>
   );
--- src/pages/CreateUserPage/index.tsx
+++ src/pages/CreateUserPage/index.tsx
@@ -1,4 +1,4 @@
-export default function UserHomePage() {
+export default function CreateUserPage() {
   return (
     <>
       <main></main>
