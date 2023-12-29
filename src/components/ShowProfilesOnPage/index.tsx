ee9e9f757dc74b4cfead3b3b6d5a30a0e2e1bbae
alexander <alexanderhardeland@gmail.com>
2023-12-29 13:24:44 +0100

    Created a component for showing the list of posts and moved the code from the search page into there and then import the component and output the list on the search page

---
+++ src/components/ShowProfilesOnPage/index.tsx
@@ -0,0 +1,30 @@
+import viewProfiles from "../../api/profiles/viewProfiles";
+import { useEffect } from "react";
+import { useState } from "react";
+import { API_SOCIAL_PROFILES, userToken } from "../../api/constants";
+import { UserProfile } from "../../api/types";
+export default function ShowProfilesOnPage() {
+  const [errorMessage, setErrorMessage] = useState<string | undefined>();
+  const [profiles, setProfiles] = useState<UserProfile[]>([]);
+  const URL: string = API_SOCIAL_PROFILES;
+  useEffect(() => {
+    const fetchProfiles = async function () {
+      try {
+        const fetchResult = await viewProfiles(URL, userToken);
+        setProfiles(fetchResult);
+      } catch (error) {
+        setErrorMessage("Something went wrong");
+      }
+    };
+    fetchProfiles();
+  }, [URL]);
+  console.log(profiles);
+  console.log(errorMessage);
+  return (
+    <>
+      {profiles.map((profile) => (
+        <div key={profile.email}>{profile.name}</div>
+      ))}
+    </>
+  );
+}
--- src/pages/UserSearchPage/index.tsx
+++ src/pages/UserSearchPage/index.tsx
@@ -1,42 +1,12 @@
-import viewProfiles from "../../api/profiles/viewProfiles";
-import { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { useState } from "react";
-import { loadUserFromLocalStorage } from "../../utils/storage";
-import { API_SOCIAL_PROFILES } from "../../api/constants";
+import ShowProfilesOnPage from "../../components/ShowProfilesOnPage";
 export default function UserSearchPage() {
-  interface UserProfile {
-    _count: {
-      posts: number;
-      followers: number;
-      following: number;
-    };
-    avatar: string;
-    banner: string;
-    email: string;
-    name: string;
-  }
-  const [errorMessage, setErrorMessage] = useState<string | undefined>();
-  const [profiles, setProfiles] = useState<UserProfile[]>([]);
-  const URL: string = API_SOCIAL_PROFILES;
-  const token: string = "token";
-  const userToken = loadUserFromLocalStorage(token);
-  useEffect(() => {
-    const fetchProfiles = async function () {
-      try {
-        const fetchResult = await viewProfiles(URL, userToken);
-        setProfiles(fetchResult);
-      } catch (error) {
-        setErrorMessage("Somthing went wrong");
-      }
-    };
-    fetchProfiles();
-  }, [URL, userToken]);
-  console.log(profiles);
-  console.log(errorMessage);
   return (
     <>
       <main>
+        <section>
+          <ShowProfilesOnPage />
+        </section>
         <div className="btn-container border-[#cbd5e1]">
           <Link to={"/user-profile-page"}>To profile page</Link>
         </div>
