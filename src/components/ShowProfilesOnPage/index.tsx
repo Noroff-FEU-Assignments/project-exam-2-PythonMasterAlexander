a03b4ce33626e973c9fe87e85557a5911ae478d2
alexander <alexanderhardeland@gmail.com>
2023-12-29 14:06:59 +0100

    Adds som psudo code in the show profiles component where I think I need to get a single profile page

--- src/components/ShowProfilesOnPage/index.tsx
+++ src/components/ShowProfilesOnPage/index.tsx
@@ -6,6 +6,7 @@ import { UserProfile } from "../../api/types";
 export default function ShowProfilesOnPage() {
   const [errorMessage, setErrorMessage] = useState<string | undefined>();
   const [profiles, setProfiles] = useState<UserProfile[]>([]);
+
   const URL: string = API_SOCIAL_PROFILES;
   useEffect(() => {
     const fetchProfiles = async function () {
@@ -20,10 +21,18 @@ export default function ShowProfilesOnPage() {
   }, [URL]);
   console.log(profiles);
   console.log(errorMessage);
+
+  //Create a fetch for /social/profiles/name
+  //The name can come from the profiles fetch, I think
   return (
     <>
       {profiles.map((profile) => (
-        <div key={profile.email}>{profile.name}</div>
+        <div key={profile.email}>
+          {profile.name}
+
+          {/*Use a onClick fuction that takes the user to the specific page by user name by clicking this button*/}
+          <button>Visit profile</button>
+        </div>
       ))}
     </>
   );
