0aef20c8d3fc2823dd8e15056837d5ce0cad7f8b
alexander <alexanderhardeland@gmail.com>
2023-12-29 08:23:32 +0100

    Changed name on a function, inserted a try/catch block on the removePost function, adds a delete post button

--- src/api/posts/removePost.ts
+++ src/api/posts/removePost.ts
@@ -2,15 +2,21 @@ export default async function removePost(
   url: string,
   token: string,
   deleteMethode: string,
-) {
-  const response = await fetch(url, {
-    method: `${deleteMethode}`,
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-    //body: JSON.stringify(),
-  });
-  console.log(response);
-  return await response.json();
+): Promise<Response> {
+  try {
+    const response = await fetch(url, {
+      method: `${deleteMethode}`,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`Error: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error("Something went wrong");
+  }
 }
--- src/pages/UserProfilePage/index.tsx
+++ src/pages/UserProfilePage/index.tsx
@@ -1,21 +1,33 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { loadUserFromLocalStorage } from "../../utils/storage";
 import { API_SOCIAL_DELETE_POST_WITH_ } from "../../api/constants";
 import { remove } from "../../api/constants";
 import removePost from "../../api/posts/removePost";
 export default function UserHomePage() {
+  const [errorMessage, setErrorMessage] = useState<string | undefined>();
   const token: string = "token";
   const userToken = loadUserFromLocalStorage(token);
   //Need to change this value to be any id the user wants to delete
-  const ACTION: string = "/9823";
+  const ACTION: string = "/9824";
   const URL: string = API_SOCIAL_DELETE_POST_WITH_ + ACTION;
-  removePost(URL, userToken, remove);
+  const clickToRemoveOnePost = async function () {
+    try {
+      const result = await removePost(URL, userToken, remove);
+      return result;
+    } catch (error) {
+      setErrorMessage("Something went wrong");
+    }
+  };
   return (
     <>
       <main>
         Hello from User profile page
         <section>
-          <button>Delete post</button>
+          <div className="btn-container">
+            <button onClick={clickToRemoveOnePost}>Delete post</button>
+          </div>
+          <div>{errorMessage && <p>{errorMessage}</p>}</div>
         </section>
         <div className="btn-container border-[#cbd5e1]">
           <Link to={"/user-home-page"}>To home page</Link>
