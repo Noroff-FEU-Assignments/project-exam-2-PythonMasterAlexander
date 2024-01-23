4fe7689d42d6bb9f609c790f4113c18e18dc65c8
alexander <alexanderhardeland@gmail.com>
2024-01-23 11:20:58 +0100

    Created a new NetSocialLogo component with logic that checks and adjust the logo size on what device size the page are

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,5 +1,8 @@
 import LogoLink from "../LogoLink";
 export default function PageHeader() {
+  //Use conditional rendering
+  //Check which page it is
+  //Return one of two components
   return (
     <>
       <header>
