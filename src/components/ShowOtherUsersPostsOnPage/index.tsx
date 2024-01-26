ce673049240f849435c3126f58100dd0627f850a
alexander <alexanderhardeland@gmail.com>
2024-01-26 18:15:37 +0100

    Working on an error on the get request to get other users posts

--- src/components/ShowOtherUsersPostsOnPage/index.tsx
+++ src/components/ShowOtherUsersPostsOnPage/index.tsx
@@ -1,27 +1,43 @@
+import viewPosts from "../../api/posts/viewPosts";
 import { useEffect, useState } from "react";
 import { UsersPosts } from "../../api/types";
-import { API_SOCIAL_POST_, userToken } from "../../api/constants";
+import { API_SOCIAL_POST_ } from "../../api/constants";
 import { Link } from "react-router-dom";
-import viewPosts from "../../api/posts/viewPosts";
+import { loadUserFromLocalStorage } from "../../utils/storage";
 export default function ShowOtherUsersPostsOnPage() {
   const [userPosts, setUserPosts] = useState<UsersPosts[]>([]);
   const [postsError, setPostsError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  //Changed the code here, now it uses the token directly from local storage instead of importing the userToken from the file ../../api/constants
+  //Also uses a if statement to see if the token exists
+  const userToken = loadUserFromLocalStorage("token");
   const URL: string = API_SOCIAL_POST_;
   useEffect(() => {
     const getPosts = async function () {
-      try {
-        const fetchPosts = await viewPosts(URL, userToken);
-        setUserPosts(fetchPosts);
-      } catch (error) {
-        setPostsError("Could not get users posts");
+      if (userToken) {
+        try {
+          const fetchPosts = await viewPosts(URL, userToken);
+          console.log("Fetched Posts:", fetchPosts);
+          setUserPosts(fetchPosts);
+        } catch (error) {
+          console.error("Error fetching posts:", error);
+          setPostsError("Could not get users posts");
+        } finally {
+          setLoading(false);
+        }
+      } else {
+        console.log("No user token");
       }
     };
     getPosts();
-  }, [URL]);
+  }, [URL, userToken]);
   return (
     <>
       <div className="main-border-styling">
-        {postsError ? (
+        {loading ? (
+          <p>Loading...</p>
+        ) : postsError ? (
           <p className="error-text-style">{postsError}</p>
         ) : (
           userPosts.map((post) => (
