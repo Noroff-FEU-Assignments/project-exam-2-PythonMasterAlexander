d52a8c427cdb6e91fb5b3c5d2382fa813a39f9a8
alexander <alexanderhardeland@gmail.com>
2024-01-27 17:19:32 +0100

    Adds react hook form on the banner and avatar

--- src/components/ShowPostMedia/index.tsx
+++ src/components/ShowPostMedia/index.tsx
@@ -1,44 +1,35 @@
 import updateMediaOnUserProfile from "../../api/profiles/updateMediaOnUserProfile";
-import { API_SOCIAL_PROFILES, userToken, user, put } from "../../api/constants";
-import { UserProfile, MediaEntry } from "../../api/types";
+import { API_SOCIAL_PROFILES, put } from "../../api/constants";
+import { MediaEntry } from "../../api/types";
 import { loadUserFromLocalStorage } from "../../utils/storage";
 import { useState } from "react";
 
-//import { useForm } from "react-hook-form";
-//import { yupResolver } from "@hookform/resolvers/yup";
-//import { updateAvatarAndBannerSchema } from "../../utils/userSchema";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { updateAvatarAndBannerSchema } from "../../utils/userSchema";
 
 export default function ShowPostMedia() {
   const [isError, setIsError] = useState<boolean>(false);
   const [errorMessage, setErrorMessage] = useState<string | undefined>();
-  const [avatarInput, setAvatarInput] = useState<string>("");
-  const [bannerInput, setBannerInput] = useState<string>("");
-
-  /*
   const {
     register,
     handleSubmit,
     reset,
     formState: { errors },
   } = useForm<MediaEntry>({
-    resolver: yupResolver(updateAvatarAndBannerSchema ),
+    resolver: yupResolver(updateAvatarAndBannerSchema),
   });
-  */
 
   const _ACTION: string = "/media";
-  const { name }: UserProfile = loadUserFromLocalStorage(user);
+  const { name } = loadUserFromLocalStorage("user");
+  const { token } = loadUserFromLocalStorage("token");
   const _USER_NAME: string = `/${name}`;
   const URL: string = API_SOCIAL_PROFILES + _USER_NAME + _ACTION;
 
-  // There is an error here, the user can send a empty request. This must be stopped using an message that the two input fields must be populated
-  // If I send two valid strings in the input fields, the request comes through but its not showing on the page
-  const changeMedia = async function () {
+  const onSubmit: SubmitHandler<MediaEntry> = async (formData) => {
     try {
-      const data: MediaEntry = {
-        banner: bannerInput,
-        avatar: avatarInput,
-      };
-      await updateMediaOnUserProfile(URL, userToken, put, data);
+      await updateMediaOnUserProfile(URL, token, put, formData);
+      reset();
     } catch (error) {
       setErrorMessage("Something went wrong");
       setIsError(true);
@@ -46,39 +37,29 @@ export default function ShowPostMedia() {
   };
   return (
     <>
-      <div className="flex flex-col gap-8">
-        <div>
-          <p className="font-poppins text-theme-color">Banner</p>
-          <input
-            value={bannerInput}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setBannerInput(e.target.value)
-            }
-            className="primary-input-style"
-          />
-          <img />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-col gap-8">
+          <div>
+            <p className="font-poppins text-theme-color">Banner</p>
+            <input {...register("banner")} className="primary-input-style" />
+            <p className="error-text-style">{errors.banner?.message}</p>
+          </div>
+          <div>
+            <p className="font-poppins text-theme-color">Avatar url</p>
+            <input {...register("avatar")} className="primary-input-style" />
+            <p className="error-text-style">{errors.avatar?.message}</p>
+          </div>
+          <div className="btn-container">
+            <button
+              type="submit"
+              className="font-poppins text-theme-color font-regular"
+            >
+              upgrade
+            </button>
+          </div>
+          {isError && <p className="error-text-style">{errorMessage}</p>}
         </div>
-        <div>
-          <p className="font-poppins text-theme-color">Avatar url</p>
-          <input
-            value={avatarInput}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setAvatarInput(e.target.value)
-            }
-            className="primary-input-style"
-          />
-          <img />
-        </div>
-        <div className="btn-container">
-          <button
-            onClick={changeMedia}
-            className="font-poppins text-theme-color font-regular"
-          >
-            upgrade
-          </button>
-        </div>
-        {isError && <p className="error-text-style">{errorMessage}</p>}
-      </div>
+      </form>
     </>
   );
 }
