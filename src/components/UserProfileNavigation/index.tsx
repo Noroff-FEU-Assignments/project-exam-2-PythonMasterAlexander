c442aa492ac398fed3862857153c60d32d970435
alexander <alexanderhardeland@gmail.com>
2024-01-29 06:06:50 +0100

    Adds default avatar image in user profile nav

--- src/components/UserProfileNavigation/index.tsx
+++ src/components/UserProfileNavigation/index.tsx
@@ -1,21 +1,19 @@
 import { Link } from "react-router-dom";
 import { loadUserFromLocalStorage } from "../../utils/storage";
+import ShowEmptyAvatar from "../ShowEmptyAvatar";
 
 export default function UserProfileNavigation() {
   const { name, avatar } = loadUserFromLocalStorage("user") || {};
-  if (!name || !avatar) {
-    return null;
-  }
 
   return (
     <>
       <img
-        src={avatar}
+        src={avatar || ShowEmptyAvatar}
         className="inline rounded-full w-11"
         alt={
           avatar
-            ? "any avatar the user have uploaded to display as user profile"
-            : ""
+            ? "User's avatar"
+            : "Default avatar for users without uploaded avatars"
         }
       />
       <span className="capitalize self-center font-poppins font-regular text-theme-color">
